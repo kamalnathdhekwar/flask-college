5e0719235d33a1972c6ab6aedfbcd56bd92df8bc
kamalnathDhekwar <123158118+kamalnathdhekwar@users.noreply.github.com>
2024-01-20 05:47:02 +0000

    flask basic

--- app.py
+++ app.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html", title="me")
+
+@app.route("/age")
+def age():
+    return "20"
+
+@app.route("/hello")
+def hello():
+    return "Hello"
